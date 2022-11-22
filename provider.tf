31321929c2bfd22afd62c88c21aa949d5464702f
danieletouke <108566435+danieletouke@users.noreply.github.com>
2022-11-21 22:46:04 -0500

    updating provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
 provider "aws" {
     region = var.my_region
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "west-cali"
+  region = "us-west-1"
+}
+
+provider "aws" {
+  alias  = "west-oreg"
+  region = "us-west-2"
+}
